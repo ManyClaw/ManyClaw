0e916591a5e3bc858741b3b4a86dd42c46896ebb
Andy R. Terrel <andy.terrel@gmail.com>
2012-02-25 18:09:51 -0600

    Adding acoustics_const runners.

---
+++ src/drivers/acoustics_const.cpp
@@ -0,0 +1,56 @@
+#include "../many_claw.h"
+
+#include "../ptwise/riemann/acoustics_const_rp.h"
+#include "../ptwise/steppers/acoustics_const/acoustics_const_rp_step_serial.h"
+#include "../ptwise/steppers/acoustics_const/acoustics_const_rp_step_serial_tiled.h"
+#include "../ptwise/steppers/acoustics_const/acoustics_const_rp_step_serial_cellwise.h"
+#include "../ptwise/steppers/acoustics_const/acoustics_const_rp_step_tbb.h"
+//#include "acoustics_const_rp_step_ispc.h"
+#include "../ptwise/steppers/acoustics_const/acoustics_const_rp_step_omp.h"
+// TODO add other step headers here
+
+int main(int argc, char ** argv)
+{
+  int nx = 100, ny = 100;
+
+  if (argc == 3)
+  {
+    nx = atoi(argv[1]);
+    ny = atoi(argv[2]);
+  }
+
+  printf("Riemann Solve on %dx%d grid\n", nx, ny);
+
+  const char * acoustics_const_rp_stepper_names[] =
+    {
+      "serial",
+      "serial_tiled",
+      "serial_cellwise",
+      "TBB",
+      "omp"
+    };
+
+  rp_step_t acoustics_const_rp_steppers[] =
+    {
+      acoustics_const_rp_step_serial,
+      acoustics_const_rp_step_serial_tiled,
+      acoustics_const_rp_step_serial_cellwise,
+      acoustics_const_rp_step_tbb,
+      acoustics_const_rp_step_omp
+      // TODO add other acoustics_const_rp_step functions here
+    };
+
+  size_t num_rp_kernels = sizeof(acoustics_const_rp_steppers)/sizeof(rp_step_t);
+
+  for (size_t i = 0; i < num_rp_kernels; i++)
+  {
+    std::cout << "Testing " << acoustics_const_rp_stepper_names[i] << " Riemann kernel...\n";
+    compare_steppers(nx, ny, acoustics_const_rp_grid_params,
+                    acoustics_const_rp_step_serial, acoustics_const_rp_steppers[i]);
+    std::cout << "  Benchmark finished in " <<
+      1e3 * benchmark_stepper(nx, ny, acoustics_const_rp_grid_params, acoustics_const_rp_steppers[i]) << " ms\n";
+  }
+
+  return 0;
+}
+
--- src/ptwise/riemann/acoustics_const_rp.h
+++ src/ptwise/riemann/acoustics_const_rp.h
@@ -1,14 +1,12 @@
 #ifndef ACOUSTICS_CONST_RP_H
 #define ACOUSTICS_CONST_RP_H
 
-#include <math.h>
-#include <stdio.h>
+#include "../../many_claw.h"
 
-typedef double real;
 const real BULK = 1.0;
 const real RHO = 1.0;
 
-typedef struct rp_params
+struct acoustics_const_rp_aux_global_t
 {
     // Bulk modules (k)
     real bulk;
@@ -18,44 +16,49 @@ typedef struct rp_params
     real c;  // = sqrt(bulk / rho);
     // Speed of sound
     real Z; // = c * rho;
-} rp_params;
+};
+
+
+static const acoustics_const_rp_aux_global_t acoustics_const_rp_aux_global = {1.0, 1.0, 1.0, 1.0};
+static const rp_grid_params acoustics_const_rp_grid_params = {2, 3, 0, 2};
 
 // Point-wise constant acoustics Riemann solver
 inline
 void acoustics_const_rp(const real* q_left, const real* q_right,
                         const real* aux_left, const real* aux_right,
-                        const rp_params* params,
+                        const acoustics_const_rp_aux_global_t* aux_global,
                         real* amdq, real* apdq, real *wave, real *s)
 {
     // Local physical constants
     real alpha[2];
-    
+    const int num_states = acoustics_const_rp_grid_params.num_states;
+
     // Wave strengths
-    alpha[0] = ( q_left[0] - q_right[0] + 
-                        params->Z * (q_right[1] - q_left[1])) / (2.0 * params->Z);
+    alpha[0] = ( q_left[0] - q_right[0] +
+                 aux_global->Z * (q_right[1] - q_left[1])) / (2.0 * aux_global->Z);
     alpha[1] = ( q_right[0] - q_left[0] +
-                        params->Z * (q_right[1] - q_left[1])) / (2.0 * params->Z);
-    
+                 aux_global->Z * (q_right[1] - q_left[1])) / (2.0 * aux_global->Z);
+
     // Wave speeds
-    s[0] = -params->c, s[1] = params->c;
- 
+    s[0] = -aux_global->c, s[1] = aux_global->c;
+
     // Set wave vectors
-    wave[0,0] = -alpha[0] * params->Z; 
-    wave[0,1] =  alpha[0];
-    wave[0,2] =  0.0;
-    
-    wave[1,0] = alpha[1] * params->Z;
-    wave[1,1] = alpha[1];
-    wave[1,2] = 0.0;
-    
+    wave[0 * num_states + 0] = -alpha[0] * aux_global->Z;
+    wave[0 * num_states + 1] =  alpha[0];
+    wave[0 * num_states + 2] =  0.0;
+
+    wave[1 * num_states + 0] = alpha[1] * aux_global->Z;
+    wave[1 * num_states + 1] = alpha[1];
+    wave[1 * num_states + 2] = 0.0;
+
     // Grid edge fluctuations
-    amdq[0] = s[0] * wave[0,0];
-    amdq[1] = s[0] * wave[0,1];
-    amdq[2] = s[0] * wave[0,2];  // This could just be set to zero
-    
-    apdq[0] = s[1] * wave[1,0];
-    apdq[1] = s[1] * wave[1,1];
-    apdq[2] = s[1] * wave[1,2];  // This could just be set to zero
+    amdq[0] = s[0] * wave[0 * num_states + 0];
+    amdq[1] = s[0] * wave[0 * num_states + 1];
+    amdq[2] = s[0] * wave[0 * num_states + 2];  // This could just be set to zero
+
+    apdq[0] = s[1] * wave[1 * num_states + 0];
+    apdq[1] = s[1] * wave[1 * num_states + 1];
+    apdq[2] = s[1] * wave[1 * num_states + 2];  // This could just be set to zero
 }
 
 #endif // ACOUSTICS_CONST_RP_H
