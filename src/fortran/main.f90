c96ea8a42a0f6ef291f899798c579ed12120e5e6
Kyle Mandli <kyle.mandli@gmail.com>
2012-02-25 18:18:00 -0600

    Add command line argument support

--- src/fortran/main.f90
+++ src/fortran/main.f90
@@ -9,11 +9,12 @@ program manyclaw
 
     implicit none
     
-    ! Test suite parameters
-    integer, parameter :: NUM_TESTS = 4
+    ! Command line arguments
+    character(len=10) :: input_nx,input_ny,input_num_tests
     
     ! Local storage
-    real(kind=TK) :: ave_time(NUM_TESTS)
+    integer :: num_tests
+    real(kind=TK) :: ave_time(4)
 
     ! Common block parameters
     real(kind=sk) :: u,v
@@ -21,32 +22,55 @@ program manyclaw
     common /advection_rp/ u,v
     common /euler_rp/  gamma,gamma1
     
-    ! Test advection Riemann solvers
+    ! Parse command line arguments
     nx = 1028
     ny = 1028
+    num_tests = 3
+    select case(iargc())
+        case(1)
+            call getarg(1,input_nx)
+            read(input_nx,'(I10)') nx
+            ny = nx
+            num_tests = 3
+        case(2)
+            call getarg(1,input_nx)
+            call getarg(2,input_ny)
+            read(input_nx,'(I10)') nx
+            read(input_ny,'(I10)') ny
+        case(3)
+            call getarg(1,input_nx)
+            call getarg(2,input_ny)
+            call getarg(3,input_num_tests)
+            read(input_nx,'(I10)') nx
+            read(input_ny,'(I10)') ny
+            read(input_num_tests,'(I10)') num_tests
+        case default
+            print *, "***ERROR*** Too many command line arguments!"
+            stop
+    end select
+    
+    ! Test advection Riemann solvers
     num_states = 1
     num_aux = 0
     num_ghost = 2
     num_waves = 1
     u = 1.0_sk
     v = 1.0_sk
-    ave_time(1) = time_rp_row_function(row_rp_advection,3)
+    ave_time(1) = time_rp_row_function(row_rp_advection,num_tests)
     print *,"Advection row Riemann solver time = ",ave_time(1)," ms."
-    ave_time(2) = time_rp_ptwise_function(ptwise_rp_advection,3)
+    ave_time(2) = time_rp_ptwise_function(ptwise_rp_advection,num_tests)
     print *,"Advection ptwise Riemann solver time = ",ave_time(2)," ms."
     
     ! Test Euler Riemann solvers
-    nx = 1028
-    ny = 1028
     num_states = 4
     num_aux = 0
     num_ghost = 2
     num_waves = 4
     gamma = 1.4_sk
     gamma1 = 1.0_sk - gamma
-    ave_time(3) = time_rp_row_function(row_rp_euler,3)
+    ave_time(3) = time_rp_row_function(row_rp_euler,num_tests)
     print *,"Euler row Riemann solver time = ",ave_time(3)," ms."
-    ave_time(4) = time_rp_ptwise_function(ptwise_rp_euler,3)
+    ave_time(4) = time_rp_ptwise_function(ptwise_rp_euler,num_tests)
     print *,"Euler row Riemann solver time = ",ave_time(4)," ms."
 
 end program manyclaw
