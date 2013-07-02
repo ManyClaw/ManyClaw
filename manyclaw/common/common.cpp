9f26de54dfcaab3ac4084e6e7aca717376305b89
Andy R. Terrel <andy.terrel@gmail.com>
2013-07-02 14:58:38 -0500

    Move advection tests together and rename foos to foo

--- manyclaw/common/common.cpp
+++ manyclaw/common/common.cpp
@@ -11,21 +11,21 @@ void compare_steppers(int nx, int ny, rp_grid_params params,
   state.randomize();
   Solution solution(grid, state);
 
-  Solver solver_1(solution, params.num_ghost, params.num_waves);
-  Solver solver_2(solution, params.num_ghost, params.num_waves);
+  Solver solver_1(solution, params.num_ghost, params.num_wave);
+  Solver solver_2(solution, params.num_ghost, params.num_wave);
 
   rp_stepper_1(&state.q[0], &state.aux[0], grid.num_cells[0], grid.num_cells[1],
-              &solver_1.amdq[0], &solver_1.apdq[0], &solver_1.waves[0],
-              &solver_1.wave_speeds[0]);
+              &solver_1.amdq[0], &solver_1.apdq[0], &solver_1.wave[0],
+              &solver_1.wave_speed[0]);
   rp_stepper_2(&state.q[0], &state.aux[0], grid.num_cells[0], grid.num_cells[1],
-              &solver_2.amdq[0], &solver_2.apdq[0], &solver_2.waves[0],
-              &solver_2.wave_speeds[0]);
+              &solver_2.amdq[0], &solver_2.apdq[0], &solver_2.wave[0],
+              &solver_2.wave_speed[0]);
 
   std::cout << "  Comparing solution to reference solution...\n";
   std::cout << "    amdq        " << max_error(solver_1.amdq, solver_2.amdq) << "\n";
   std::cout << "    apdq        " << max_error(solver_1.apdq, solver_2.apdq) << "\n";
-  std::cout << "    waves       " << max_error(solver_1.waves, solver_2.waves) << "\n";
-  std::cout << "    wave_speeds " << max_error(solver_1.wave_speeds, solver_2.wave_speeds) << "\n";
+  std::cout << "    wave       " << max_error(solver_1.wave, solver_2.wave) << "\n";
+  std::cout << "    wave_speed " << max_error(solver_1.wave_speed, solver_2.wave_speed) << "\n";
 }
 
 double benchmark_stepper(int nx, int ny, rp_grid_params params, rp_step_t rp_stepper)
@@ -36,12 +36,12 @@ double benchmark_stepper(int nx, int ny, rp_grid_params params, rp_step_t rp_ste
   State state(grid, params.num_eqn, params.num_aux, params.num_ghost);
   state.randomize();
   Solution solution(grid, state);
-  Solver solver(solution, params.num_ghost, params.num_waves);
+  Solver solver(solution, params.num_ghost, params.num_wave);
 
   timer t;
   rp_stepper(&state.q[0], &state.aux[0], grid.num_cells[0], grid.num_cells[1],
-             &solver.amdq[0], &solver.apdq[0], &solver.waves[0],
-             &solver.wave_speeds[0]);
+             &solver.amdq[0], &solver.apdq[0], &solver.wave[0],
+             &solver.wave_speed[0]);
   double time = t.elapsed();
 
   return time;
@@ -72,15 +72,15 @@ double compare_updates(int nx, int ny, rp_grid_params params,
   ref_state.randomize();
  
   Solution solution(grid, state);
-  Solver solver(solution, params.num_ghost, params.num_waves);
+  Solver solver(solution, params.num_ghost, params.num_wave);
 
   rp_stepper(&state.q[0], &state.aux[0], grid.num_cells[0], grid.num_cells[1],
-              &solver.amdq[0], &solver.apdq[0], &solver.waves[0],
-              &solver.wave_speeds[0]);
+              &solver.amdq[0], &solver.apdq[0], &solver.wave[0],
+              &solver.wave_speed[0]);
 
   updater(&state.q[0], &state.aux[0], grid.num_cells[0], grid.num_cells[1], 
-          &solver.amdq[0],  &solver.apdq[0],  &solver.waves[0], 
-          &solver.wave_speeds[0], params.num_ghost, params.num_eqn, 1.0);
+          &solver.amdq[0],  &solver.apdq[0],  &solver.wave[0], 
+          &solver.wave_speed[0], params.num_ghost, params.num_eqn, 1.0);
 
   return max_error(ref_state.q, state.q);
 }
--- manyclaw/grid_eval/manyclaw_grid_eval.h
+++ manyclaw/grid_eval/manyclaw_grid_eval.h
@@ -1,9 +1,6 @@
 #ifndef __MANYCLAW_GRID_EVAL_H
 #define __MANYCLAW_GRID_EVAL_H
 
-#include <manyclaw/grid_eval/advection/manyclaw_grid_eval_advection.h>
-#include <manyclaw/grid_eval/acoustics_const/manyclaw_grid_eval_acoustics_const.h>
-#include <manyclaw/grid_eval/acoustics_var/manyclaw_grid_eval_acoustics_var.h>
-#include <manyclaw/grid_eval/euler/manyclaw_grid_eval_euler.h>
+#include <manyclaw/grid_eval/advection_grid_eval.h>
 
 #endif __MANYCLAW_GRID_EVAL_H
