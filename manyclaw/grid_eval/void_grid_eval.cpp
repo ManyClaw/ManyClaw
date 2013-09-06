7c3aacead90169b1e42560c591b1469eea197c7d
Kyle Mandli <kyle.mandli@gmail.com>
2013-09-06 16:29:58 -0500

    Change indexing to loop over only needed cell edges
    Elliminate evaluation of Riemann problems in corner cells that are not
    expected to be set properly.

--- manyclaw/grid_eval/void_grid_eval.cpp
+++ manyclaw/grid_eval/void_grid_eval.cpp
@@ -1,5 +1,8 @@
 #include "void_grid_eval.h"
 
+// TODO: Remove cout
+#include <iostream>
+
 // Evaluates void_rp via serial execution
 void void_rp_grid_eval_serial(rp_t rp,
                               rp_grid_params_t rp_grid_params,
@@ -14,10 +17,10 @@ void void_rp_grid_eval_serial(rp_t rp,
                               real* wave_speed)
 {
   unsigned col, row;
-  const int num_ghost = rp_grid_params.num_ghost;
-  const int num_eqn = rp_grid_params.num_eqn;
-  const int num_aux = rp_grid_params.num_aux;
-  const int num_wave = rp_grid_params.num_wave;
+  const unsigned num_ghost = rp_grid_params.num_ghost;
+  const unsigned num_eqn = rp_grid_params.num_eqn;
+  const unsigned num_aux = rp_grid_params.num_aux;
+  const unsigned num_wave = rp_grid_params.num_wave;
 
   FieldIndexer fi(nx, ny, num_ghost, num_eqn);
   FieldIndexer fi_aux(nx, ny, num_ghost, num_aux);
@@ -26,13 +29,17 @@ void void_rp_grid_eval_serial(rp_t rp,
   EdgeFieldIndexer efi_asdq(nx, ny, num_ghost, num_eqn);
   EdgeFieldIndexer efi_s(nx, ny, num_ghost, num_wave);
 
-  for(row = 1; row < efi_wave.num_row_edge_transverse(); ++row){
-    for(col = 1; col < efi_wave.num_col_edge_normal() + 1; ++col) {
+  // Interior of domain minus the top and right boundaries
+  for(row = num_ghost; row < ny + num_ghost; ++row) {
+    for(col = num_ghost; col < nx + num_ghost; ++col) {
+
+      // Left edge of cell
       rp(q + fi.idx(row, col-1), q + fi.idx(row, col),
          aux + fi_aux.idx(row, col-1), aux + fi_aux.idx(row, col), aux_global, 0,
          amdq + efi_asdq.left_edge(row, col), apdq + efi_asdq.left_edge(row, col),
          wave + efi_wave.left_edge(row, col), wave_speed + efi_s.left_edge(row, col));
 
+      // Bottom edge of cell
       rp(q + fi.idx(row-1, col), q + fi.idx(row, col),
          aux + fi_aux.idx(row-1, col), aux + fi_aux.idx(row, col), aux_global, 1,
          amdq + efi_asdq.down_edge(row, col), apdq + efi_asdq.down_edge(row, col),
@@ -40,20 +47,48 @@ void void_rp_grid_eval_serial(rp_t rp,
     }
   }
 
-  for(col = 1; col < efi_wave.num_col_edge_transverse(); ++col) {
-    row = efi_wave.num_row_edge_transverse();
-    rp(q + fi.idx(row - 1, col), q + fi.idx(row, col),
-       aux + fi_aux.idx(row - 1, col), aux + fi_aux.idx(row, col), aux_global, 1,
-       amdq + efi_asdq.down_edge(row, col), apdq + efi_asdq.down_edge(row, col),
-       wave + efi_wave.down_edge(row, col), wave_speed + efi_s.down_edge(row, col));
+  // Bottom boundary ghost cells
+  for(row = 0; row < num_ghost - 1; ++row) {
+    for(col = num_ghost; col < nx + num_ghost; ++col) {
+
+      rp(q + fi.idx(row, col), q + fi.idx(row+1, col),
+         aux + fi_aux.idx(row-1, col), aux + fi_aux.idx(row, col), aux_global, 1,
+         amdq + efi_asdq.up_edge(row, col), apdq + efi_asdq.up_edge(row, col),
+         wave + efi_wave.up_edge(row, col), wave_speed + efi_s.up_edge(row, col));
+    }  
   }
 
-  for(row = 1; row < efi_wave.num_row_edge_transverse(); ++row){
-    col = efi_wave.num_col_edge_transverse();
-    rp(q + fi.idx(row, col - 1), q + fi.idx(row, col),
-       aux + fi_aux.idx(row, col - 1), aux + fi_aux.idx(row, col), aux_global, 0,
-       amdq + efi_asdq.left_edge(row, col), apdq + efi_asdq.left_edge(row, col),
-       wave + efi_wave.left_edge(row, col), wave_speed + efi_s.left_edge(row, col));
+  // Left boundary ghost cells
+  for(row = num_ghost; row < ny + num_ghost; ++row) {
+    for (col = 0; col < num_ghost - 1; ++col) {
+
+      rp(q + fi.idx(row, col), q + fi.idx(row+1, col),
+         aux + fi_aux.idx(row, col), aux + fi_aux.idx(row, col + 1), aux_global, 1,
+         amdq + efi_asdq.right_edge(row, col), apdq + efi_asdq.right_edge(row, col),
+         wave + efi_wave.right_edge(row, col), wave_speed + efi_s.right_edge(row, col));
+    }
+  }
+
+  // Right boundary ghost cells + right boundary edge
+  for(row = num_ghost; row < ny + num_ghost; ++row) {
+    for (col = nx + num_ghost; col < nx + 2 * num_ghost; ++col) {
+
+      rp(q + fi.left(row, col), q + fi.idx(row, col),
+         aux + fi_aux.idx(row, col), aux + fi_aux.idx(row, col + 1), aux_global, 1,
+         amdq + efi_asdq.left_edge(row, col), apdq + efi_asdq.left_edge(row, col),
+         wave + efi_wave.left_edge(row, col), wave_speed + efi_s.left_edge(row, col));
+    }
+  }
+
+  // Top boundary ghost cells + top boundary edge
+  for(row = ny + num_ghost; row < ny + 2 * num_ghost; ++row) {
+    for (col = num_ghost; col < nx + num_ghost; ++col) {
+
+      rp(q + fi.idx(row - 1, col), q + fi.idx(row - 1, col),
+         aux + fi_aux.idx(row, col), aux + fi_aux.idx(row, col + 1), aux_global, 1,
+         amdq + efi_asdq.down_edge(row, col), apdq + efi_asdq.down_edge(row, col),
+         wave + efi_wave.down_edge(row, col), wave_speed + efi_s.down_edge(row, col));
+    }
   }
 }
 
